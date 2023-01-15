9dc1baaee69a6a30a8efa7496f134595d3d5b9ea
Amit Swisa <amitswisa5@gmail.com>
2023-01-15 15:26:11 +0200

    Routing and Mobile fixed

--- src/Components/NavbarComponent.js
+++ src/Components/NavbarComponent.js
@@ -1,12 +1,19 @@
 import LinkNavigation from "./LinkNavigation";
+import { useLocation } from "react-router-dom";
 
 function NavbarComponent() {
+  const location = useLocation();
+
   return (
     <>
       <div className="Navbar buttonGroup d-flex flex-row">
         <LinkNavigation link="/" text="Home" />
-        <LinkNavigation link="/Login" text="Login" />
-        <LinkNavigation link="/Register" text="Register" />
+        {location.pathname !== "/Login" && (
+          <LinkNavigation link="/Login" text="Login" />
+        )}
+        {location.pathname !== "/Register" && (
+          <LinkNavigation link="/Register" text="Register" />
+        )}
       </div>
     </>
   );
