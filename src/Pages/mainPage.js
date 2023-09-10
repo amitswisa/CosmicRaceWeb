43c6807077b9b9c5dacfab6598fd78d2f86b66a1
Amit Swisa <amitswisa5@gmail.com>
2023-09-10 22:52:03 +0300

    URL of download change

--- src/Pages/mainPage.js
+++ src/Pages/mainPage.js
@@ -19,7 +19,7 @@ const MainPage = () => {
         <LinkNavigation link="/Register" text="Register with new account" />
         <LinkNavigation link="/room" text="Play as a guest" />
         <LinkNavigation
-          link="http://localhost:3001/CosmicRace-installer-1.0.exe"
+          link="http://cosmicrace.tech/CosmicRace-installer-1.0.exe"
           text="Download CosmicRace"
         />
       </div>
